5a1b79209adb15d116b616e32b28ba1ef674e5c1
Csaba Koncz <ckoncz@cloudera.com>
2020-01-21 08:59:37 +0100

    Workaround for transpiling bug in systemjs

--- src/app/app-state.ts
+++ src/app/app-state.ts
@@ -1,3 +1,10 @@
+/*
+ here we must have something that makes this module recognized as an ES6 module by SystemJS
+https://github.com/systemjs/systemjs/blob/0.21.6/src/instantiate.js#L417
+Typescript interface and enum are not in the regex
+*/
+export const STATE = 'STATE' //do not remove this line
+
 export interface Todo{
     text: string
     completed: boolean
