6a7599bbb090297812adcad428655a3767d656d9
Csaba Koncz <ckoncz@cloudera.com>
2020-02-12 09:08:28 +0100

    Fix SystemJS lazy loading
    TODO: relative import from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,27 @@ import { RouterModule, Routes } from '@angular/router'
 import { ComponentsModule, AppComponent } from './components/index';
 import {AppStoreService} from './services/index';
 
+declare const SystemJS: any
+
 const routes: Routes=[
   {
     path: 'subfooter',
-    loadChildren: () => import('./components/subfooter.module').then(m => m.SubfooterModule)
+    loadChildren: () => {
+      var mp
+      if(window['SystemJS']){
+        //TODO: learn to make relative imports with SystemJS
+        mp=SystemJS.import('app/components/subfooter.module')
+      }
+      else{
+        mp = import('./components/subfooter.module')
+      }
+      return mp.then(m => m.SubfooterModule)
+    }
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
   }
 ]
 @NgModule({
